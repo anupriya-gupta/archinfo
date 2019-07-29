91ba8cf210a7ba1bbe2cedb4c4ae3eef3684065e
Anupriya Gupta <anupriya-gupta@users.noreply.github.com>
2019-07-29 02:10:12 -0700

    Update test_register.py

--- tests/test_register.py
+++ tests/test_register.py
@@ -3,24 +3,24 @@ def test_register():
 
     register = Register( name = 'MIPS', size = 8, vex_offset = 10,
                          vex_name = 'xyz', subregisters = ['a', 'b'],
-                         alias_names = ('r0'), general_purpose = True, floating_point = True,
+                         alias_names = ('r0'), general_purpose=True, floating_point = True,
                          vector = True, argument = True, persistent = True,
-                         default_value = (True, 'global'), linux_entry_value = 'argv',
-                         concretize_unique = True, concrete = True, artificial = True )
+                         default_value = ('a', 'global'), linux_entry_value = 'argv',
+                         concretize_unique = True, concrete = True, artificial = True)
     assert register.name == 'MIPS'
     assert register.size == 8 
     assert register.vex_offset == 10
     assert register.vex_name == "xyz"
     assert register.subregisters == ['a', 'b']
     assert register.alias_names == ('r0')
-    assert register.general_purpose == True
-    assert register.floating_point == True
-    assert register.vector == True
-    assert register.argument == True
-    assert register.persistent == True 
-    assert register.default_value == (True, 'global')
+    assert register.general_purpose
+    assert register.floating_point 
+    assert register.vector 
+    assert register.argument 
+    assert register.persistent 
+    assert register.default_value == ('a', 'global')
     assert register.linux_entry_value == 'argv'
-    assert register.concretize_unique == True
-    assert register.concrete == True
-    assert register.artificial == True
+    assert register.concretize_unique 
+    assert register.concrete
+    assert register.artificial
 
